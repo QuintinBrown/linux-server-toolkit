638d2acc10d86a72f2451509dae4956978bb5574
QuintinBrown <quintin.brown.m@gmail.com>
2026-05-09 00:05:51 -0400

    plugin selection and network_testing(run command and ping gateway)

--- core/server_toolkit.py
+++ core/server_toolkit.py
@@ -8,6 +8,12 @@ def fatal(error):
     print(f"[FATAL] {error}")
     exit(1)
 
+def error(description):
+    print(f"[ERROR] {description}")
+
+def quit():
+    exit(0)
+
 def start_CLI():
     print_plugins()
 
@@ -24,6 +30,25 @@ def print_plugins():
 
         for idx, command in enumerate(commands):
             print(f"{idx+1}. {command}")
+
+        option = input("\nSelect an option: ")
+
+        if option == "q":
+            quit()
+
+        try:
+            option = int(option)
+        except ValueError:
+            error("Invalid option")
+            return
+        
+        command_list = list(commands.items())
+        if option < 1 or option > len(command_list):
+            error("Invalid option")
+            return
+
+        command_name, command_func = command_list[option - 1]
+        command_func()
 
 def arg_parsing():
     parser = argparse.ArgumentParser()
--- plugins/network_testing.py
+++ plugins/network_testing.py
@@ -1,3 +1,6 @@
+import subprocess
+import re
+
 from core.base_plugin import BasePlugin
 
 class Plugin(BasePlugin):
@@ -8,11 +11,25 @@ class Plugin(BasePlugin):
             "Ping Gateway": self.ping_gateway,
             "Remote Ping": self.ping_remote,
             "Ping DNS": self.ping_dns,
-            "Test DNS": self.test_dns
+            "Test DNS": self.test_dns,
+            "Find Gateway": self.find_gateway
         }
+    
+    def run(self, cmd):
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+
+    def find_gateway(self, want_printed=True):
+        result = self.run("ip route")
+        pretty_result = re.search(r'\bvia\s([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', result)
+        if want_printed:
+            print(pretty_result.group(1))
+        else:
+            return pretty_result.group(1)
     
     def ping_gateway(self):
         print("TODO: Ping Gateway")
+
 
     def ping_remote(self):
         print("TODO: Ping Remote")
